e2025f5edf454f55c43c98d5b02db03b96d210cf
CodePrometheus <Code_Prince@outlook.com>
2021-10-03 19:45:41 +0800

    feat

---
+++ python-server/peewee_test/model_define.py
@@ -0,0 +1,80 @@
+import datetime
+import logging
+
+from peewee import *
+
+logger = logging.getLogger('peewee')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+db = MySQLDatabase('starry-supermarket', host='localhost',
+                   user='root', passwd='root')
+
+
+class User(Model):
+    username = CharField(unique=True)
+
+    class Meta:
+        database = db
+
+
+class Tweet(Model):
+    user = ForeignKeyField(User, backref='tweets')
+    message = TextField()
+    created_date = DateTimeField(default=datetime.datetime.now())
+    is_published = BooleanField(default=True)
+
+    class Meta:
+        database = db
+
+
+if __name__ == '__main__':
+    db.connect()
+    db.create_tables([User, Tweet])
+
+    # 2. 添加
+    charlie = User(username="charlie")  # update set age=18 where username="charlie"
+    rows = charlie.save()  # 1. save方法既可以完成新建，也可以完成更新的操作(你的对象中主键值是否有设置，你是一个更新的操作)
+    if rows == 0:
+        print("未更新数据")
+    huey = User.create(username="huey")
+
+    # 3. 查询
+    # 1. get方法 - 1. 返回来的是直接的user对象 2. 这个方法如果查询不到会抛出异常
+    try:
+        # charlie = User.get(User.username=="charlie")
+        charlie = User.get_by_id("charlie")
+        print(charlie.username)
+        # 这个操作发起的sql请求是什么
+    except User.DoesNotExist as e:
+        print("查询不到")
+
+    # 2. 查询所有
+    users = User.select()  # 1. 没有看到sql查询语句，用于组装sql 2. 对象是ModelSelect 我们对ModelSelect进行for循环和切片的时候才会发起请求
+    # print(users.sql())
+    print(type(users))
+    user = users[0]
+    print(type(user))
+
+    usernames = ["charlie", "huey", "mickey"]
+    users = User.select().where(User.username.in_(usernames))
+    for user in users:
+        print(user.username)
+    for user in User.select():
+        print(user.username)
+
+    # 更新
+    charlie = User(username="charlie")  # update set xx=xx where username="charlie"
+    print(charlie.save())
+    # 使用update更新
+    print(User.update(age=20).where(User.username == "charlie").execute())
+
+    # 删除数据
+    user = User.get(User.username == "huey")
+    user.delete_instance()
+
+    # 是不是也不会执行
+    query = User.delete().where(User.username == "charlie").execute()
+    print(query)
+
+    # 有少数的方法会直接执行sql语句 get get_by_id
---
+++ python-server/peewee_test/more_define.py
@@ -0,0 +1,83 @@
+import datetime
+import logging
+import random
+
+from peewee import *
+
+logger = logging.getLogger("peewee")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+db = MySQLDatabase('starry-supermarket', host='localhost',
+                   user='root', passwd='root')
+
+
+class BaseModel(Model):
+    add_time = DateTimeField(default=datetime.datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        database = db  # 这里是数据库链接，为了方便建立多个表，可以把这个部分提炼出来形成一个新的类
+
+
+class Person(BaseModel):
+    first = CharField()
+    last = CharField()
+
+    class Meta:
+        primary_key = CompositeKey('first', 'last')
+
+
+class Pet(BaseModel):
+    owner_first = CharField()
+    owner_last = CharField()
+    pet_name = CharField()
+
+    class Meta:
+        constraints = [SQL('FOREIGN KEY(owner_first, owner_last) REFERENCES person(first, last)')]
+
+
+class Blog(BaseModel):
+    pass
+
+
+class Tag(BaseModel):
+    pass
+
+
+# 复合主键
+class BlogToTag(BaseModel):
+    """A simple "through" table for many-to-many relationship."""
+    blog = ForeignKeyField(Blog)
+    tag = ForeignKeyField(Tag)
+
+    class Meta:
+        primary_key = CompositeKey('blog', 'tag')
+
+
+class User(BaseModel):
+    # 如果没有设置主键，那么自动生成一个id的主键
+    username = CharField(max_length=20)
+    age = CharField(default=18, max_length=20, verbose_name="年龄")
+
+    class Meta:
+        table_name = 'new_user'  # 这里可以自定义表名
+
+
+if __name__ == "__main__":
+    db.connect()
+    db.create_tables([Person, Pet, Blog, Tag, BlogToTag, User])
+
+    # id = Person.insert({
+    #     'first': 'star',
+    #     'last': '2021'
+    # }).execute()
+    #
+    # for i in range(10):
+    #     User.create(username=f"2021-{i}", age=random.randint(18, 40))
+    #
+    # id = Blog.insert({}).execute()
+    # print(id)
+
+    # and
+    person = Person.select().where((Person.first == "li") & (Person.first == "li1"))
+    print(person.sql())
